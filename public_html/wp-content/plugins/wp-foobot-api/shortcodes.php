84cbc9e424c1d76fd0beedffb66ad7f4f9cbb39b
markbain <markbaindesign@gmail.com>
2019-11-12 13:52:28 +0100

    add shortcodes and basic functions to get temperature data

---
+++ public_html/wp-content/plugins/wp-foobot-api/shortcodes.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ * Shortcodes
+ */
+
+function bd_foobot_show_api_key()
+{
+   
+   $key = bd_foobot_get_api_key();
+   ob_start();
+	echo '<pre><code>'. $key . '</code></pre>';
+	$content =  ob_get_contents();
+	ob_clean();
+	return $content;
+}
+add_shortcode('foobot_show_key', 'bd_foobot_show_api_key');
+
+function bd_foobot_show_device_data()
+{
+   
+   $device = bd_get_foobot_device();
+   ob_start();
+	echo '<pre><code>';
+	var_dump( $device );
+	echo '</code></pre>';
+	$content =  ob_get_contents();
+	ob_clean();
+	return $content;
+}
+add_shortcode('foobot_show_device', 'bd_foobot_show_device_data');
+
+/**
+ * Show device name
+ */
+function bd_foobot_show_device_name()
+{   
+	$name = bd_get_foobot_device_name();
+   ob_start();
+	echo $name;
+	$content =  ob_get_contents();
+	ob_clean();
+	return $content;
+}
+add_shortcode('foobot_device_name', 'bd_foobot_show_device_name');
+
+/**
+ * Show Foobot device data
+ */
+function bd_foobot_show_data_from_device( $device )
+{   
+   $device = bd_get_foobot_data();
+   ob_start();
+	echo '<pre><code>';
+	var_dump( $device );
+	echo '</code></pre>';
+	$content =  ob_get_contents();
+	ob_clean();
+	return $content;
+}
+add_shortcode('foobot_device_data', 'bd_foobot_show_data_from_device');
+
+/**
+ * Show temp now
+ */
+function bd_foobot_show_temp_now( $device )
+{   
+   $data = bd_get_temp_now();
+	ob_start();
+	echo '<div>Current temperature:</div>';
+	echo '<h1>' . $data . '</h1>';
+	$content =  ob_get_contents();
+	ob_clean();
+	return $content;
+}
+add_shortcode('foobot_temp_now', 'bd_foobot_show_temp_now');
--- public_html/wp-content/plugins/wp-foobot-api/wp-foobot-api.php
+++ public_html/wp-content/plugins/wp-foobot-api/wp-foobot-api.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Foobot API plugin
  * Plugin URI: https://bain.design/wp-foobot-api
@@ -12,9 +13,134 @@
  * Plugin Slug: wp-foobot-api
  */
 
-function baindesign_foobot_plugin_init() {
+function baindesign_foobot_plugin_init()
+{
 
-	include( plugin_dir_path( __FILE__ ) . 'admin/admin.php');
+	include(plugin_dir_path(__FILE__) . 'admin/admin.php');
+	include(plugin_dir_path(__FILE__) . 'shortcodes.php');
+
+	function bd_foobot_get_api_key()
+	{
+		$options = get_option('baindesign_foobot_api_settings');
+		return $options['baindesign_foobot_api_key'];
+	}
+
+	function bd_get_foobot_device()
+	{
+		$key = bd_foobot_get_api_key();
+
+		// echo '<pre><code>';
+		// var_dump( $key );
+		// echo '</code></pre>';
+
+		$url = 'https://api.foobot.io/v2/owner/mark@bain.design/device/';
+		$args = array(
+			'headers' => array(
+				'X-API-KEY-TOKEN' => $key
+			)
+		);
+
+		$request = wp_remote_get( $url, $args );
+
+		// echo '<pre><code>';
+		// var_dump( $request );
+		// echo '</code></pre>';
+
+		if (is_wp_error($request)) {
+			return false; // Bail early
+		}
+
+		$body = wp_remote_retrieve_body($request);
+
+		$api_data = json_decode($body);
+		return $api_data;
+	}
+
+	/**
+	 * Get the API data
+	 * 
+	 * Now that we have the device UUID, we can call for 
+	 * the data from the device. 
+	 * 
+	 */
+	function bd_get_foobot_data()
+	{
+		$key = bd_foobot_get_api_key();
+		$uuid = bd_get_foobot_device_uuid();
+
+		// echo '<pre><code>';
+		// var_dump( $key );
+		// echo '</code></pre>';
+
+		$url = 'https://api.foobot.io/v2/device/' . $uuid . '/datapoint/0/last/0/?' . $key;
+		$args = array(
+			'headers' => array(
+				'X-API-KEY-TOKEN' => $key
+			)
+		);
+
+		$request = wp_remote_get( $url, $args );
+
+		// echo '<pre><code>';
+		// var_dump( $request );
+		// echo '</code></pre>';
+
+		if (is_wp_error($request)) {
+			return false; // Bail early
+		}
+
+		$body = wp_remote_retrieve_body($request);
+
+		$api_data = json_decode($body);
+		return $api_data;
+	}
+
+	/**
+	 * Get device names
+	 */
+	function bd_get_foobot_device_name()
+	{
+		$device = bd_get_foobot_device();
+		$name = $device[0]->{"name"};
+		return $name;
+	}
+
+	/** 
+	 * Get device UUID
+	 */
+	function bd_get_foobot_device_uuid()
+	{
+		$device = bd_get_foobot_device();
+		$uuid = $device[0]->{"uuid"};
+		return $uuid;
+	}
+
+	/** 
+	 * Get current temperature
+	 */
+	function bd_get_temp_now()
+	{
+		$data = bd_get_foobot_data();
+
+		// Get the temp
+		$datapoints = $data->{"datapoints"};
+		$datapoint = $datapoints[0];
+		$temp = $datapoint[2];
+
+		// Rounding
+		$tr = round($temp, 1);
+
+		// Get the units
+		$units = $data->{"units"};
+
+		$u = $units[2];
+
+		// Concat output
+		$content = $tr.$u;
+		
+		return $content;
+
+	}
 
 }
-add_action( 'plugins_loaded', 'baindesign_foobot_plugin_init' );
+add_action('plugins_loaded', 'baindesign_foobot_plugin_init');
