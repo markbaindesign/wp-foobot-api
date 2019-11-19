6f84a23b501e7c130870a5b37ab2739dc123381c
markbain <markbaindesign@gmail.com>
2019-11-19 18:21:03 +0100

    fix indents

--- public_html/wp-content/plugins/wp-foobot-api/database.php
+++ public_html/wp-content/plugins/wp-foobot-api/database.php
@@ -9,56 +9,56 @@ $bd_foobot_db_version = '1.0';
 
 function bd_foobot_create_table()
 {
-   global $wpdb;
-   global $bd_foobot_db_version;
+	global $wpdb;
+	global $bd_foobot_db_version;
 
-   $table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
+	$table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
 
-   $charset_collate = $wpdb->get_charset_collate();
+	$charset_collate = $wpdb->get_charset_collate();
 
-   $sql = "CREATE TABLE $table_name (
+	$sql = "CREATE TABLE $table_name (
 		id mediumint(9) NOT NULL AUTO_INCREMENT,
 		time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-      device tinytext NOT NULL,
+		device tinytext NOT NULL,
 		unitTime tinytext NOT NULL,
-      datapointTime float NOT NULL,
+		datapointTime float NOT NULL,
 		unitPm tinytext NOT NULL,
-      datapointPm float NOT NULL,
+		datapointPm float NOT NULL,
 		unitTmp tinytext NOT NULL,
-      datapointTmp float NOT NULL,
+		datapointTmp float NOT NULL,
 		unitHum tinytext NOT NULL,
-      datapointHum float NOT NULL,
+		datapointHum float NOT NULL,
 		unitCo2 tinytext NOT NULL,
-      datapointCo2 float NOT NULL,
-      unitVoc tinytext NOT NULL,
-      datapointVoc float NOT NULL,
-      unitAllpollu tinytext NOT NULL,
-      datapointAllpollu float NOT NULL,
+		datapointCo2 float NOT NULL,
+		unitVoc tinytext NOT NULL,
+		datapointVoc float NOT NULL,
+		unitAllpollu tinytext NOT NULL,
+		datapointAllpollu float NOT NULL,
 		PRIMARY KEY  (id)
 	) $charset_collate;";
 
-   require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-   dbDelta($sql);
+	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+	dbDelta($sql);
 
-   add_option('bd_foobot_db_version', $bd_foobot_db_version);
+	add_option('bd_foobot_db_version', $bd_foobot_db_version);
 }
 
 function bd_foobot_install_data()
 {
-   global $wpdb;
+	global $wpdb;
 
-   $table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
+	$table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
 
-   $wpdb->insert(
-      $table_name,
-      array(
-         'time' => '0000-00-00 00:00:00',
-         'device' => 'MyDevice',
-         'sensor' => 'tmp',
-         'unit' => 'C',
-         'datapoint' => '19.248',
-      )
-   );
+	$wpdb->insert(
+		$table_name,
+		array(
+			'time' => '0000-00-00 00:00:00',
+			'device' => 'MyDevice',
+			'sensor' => 'tmp',
+			'unit' => 'C',
+			'datapoint' => '19.248',
+		)
+	);
 }
 
 /**
@@ -66,35 +66,35 @@ function bd_foobot_install_data()
  */
 function bd_foobot_update_temp_data()
 {
-   global $wpdb;
+	global $wpdb;
 
-   $welcome_name = 'Mr. WordPress';
-   $welcome_text = 'Congratulations, you just completed the installation!';
+	$welcome_name = 'Mr. WordPress';
+	$welcome_text = 'Congratulations, you just completed the installation!';
 
-   $table_name = $wpdb->prefix . 'bd_foobot_data';
+	$table_name = $wpdb->prefix . 'bd_foobot_data';
 
-   $wpdb->insert(
-      $table_name,
-      array(
-         'time' => current_time('mysql'),
-         'name' => $welcome_name,
-         'text' => $welcome_text,
-      )
-   );
+	$wpdb->insert(
+		$table_name,
+		array(
+			'time' => current_time('mysql'),
+			'name' => $welcome_name,
+			'text' => $welcome_text,
+		)
+	);
 }
 
 /**
  * Fetch data for a particular sensor from the database
  */
 function bd_foobot_fetch_latest_sensor_data(){
-   
-   // Vars
-   global $wpdb;
-   $table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
-   
-   // $data = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM `{$table_name}` ORDER BY `id` DESC LIMIT 1", $sensor) );
-   $data = $wpdb->get_row( "SELECT * FROM `{$table_name}` ORDER BY `id` DESC LIMIT 1", ARRAY_A );
+	
+	// Vars
+	global $wpdb;
+	$table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
+	
+	// $data = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM `{$table_name}` ORDER BY `id` DESC LIMIT 1", $sensor) );
+	$data = $wpdb->get_row( "SELECT * FROM `{$table_name}` ORDER BY `id` DESC LIMIT 1", ARRAY_A );
 
-   return $data;
+	return $data;
 
 }
