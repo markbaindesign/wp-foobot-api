54dbed0d0dcfc54ae221a68d35f3a8502474b26e
Mark <Bain>
2019-12-06 19:34:44 +0100

    begin work on functions to add sensor data to the db

--- public_html/wp-content/plugins/wp-foobot-api/includes/database.php
+++ public_html/wp-content/plugins/wp-foobot-api/includes/database.php
@@ -29,10 +29,8 @@ function bd_foobot_get_api_key()
 // Get API username from the database
 function bd_foobot_get_api_user()
 {
-	// TO DO
-   // $options = get_option('baindesign_foobot_api_settings');
-	// return $options['baindesign_foobot_api_key'];
-	return 'mark@bain.design'; // Temp
+   $options = get_option('baindesign_foobot_api_settings');
+	return $options['baindesign_foobot_api_user'];
 }
 
 /**
@@ -191,120 +189,97 @@ function bd_foobot_update_db_device( $device_api_data ){
    }
 }
 
-function bd_foobot_update_device_data(){
+/**
+ * Add sensor data to database
+ */
+function bd_foobot_update_db_sensors( $api_data ){
 
-   // Get the API data
+   global $wpdb;
+   
+   // DEBUG
+   $wpdb->show_errors(); // Turn on errors display
+
+   $table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
+   $time = current_time('timestamp');
+
+   // Loop through each sensor
+   foreach( $api_data as $data ){
+      $db_data = array();
+
+      // Loop the specific sensor data
+      foreach ( $data as $key => $value ){
+         $db_data[] = array( $key => $value );
+      }
+      
+      // vars
+      $uuid    = $db_data[0]['uuid'];
+      $userId  = $db_data[1]['userId'];
+      $mac     = $db_data[2]['mac'];
+      $name    = $db_data[3]['name'];
+
+      //echo '<pre><code>';
+      //var_dump( $uuid );
+      //echo '</code></pre>';
+      
+      // Insert data into db table
+      $wpdb->insert( 
+         $table_name, 
+         array(
+            'timestamp'    => $time,
+            'name'         => $name,
+            'uuid'         => $uuid,
+         ),
+         array(
+            '%d',
+            '%s',
+            '%s'
+         )
+      );
+
+      error_log("EVENT: Sensor data inserted in table", 0);
+
+      // DEBUG
+      $wpdb->print_error(); // Show error if any
+
+   }
+}
+
+function bd_foobot_update_device_data(){
+   /**
+    * Request an API call
+    * (checks if transient set, if
+    * not, makes API call)
+    */
    $data = bd_foobot_get_device_data();
 
-   //echo '<pre><code>';
-   //var_dump( $data );
-   //echo '</code></pre>';
-   
-   // Update the database with the API data
-   bd_foobot_update_db_device($data);
+   if( $data ){
+      /**
+       * If the request returns data
+       * (i.e. transient not set)
+       * update the database
+       */
+      bd_foobot_update_db_device( $data );
+   }
 
 }
 
-// Update sensor data
 function bd_foobot_update_sensor_data()
 {
-   global $wpdb;
-
-   // If an API call has been made within the last 5 mins, 
-   // return.
-   if (1 == get_transient('foobot-api-data-updated')) {
-      // Debug
-      error_log("No Foobot API call made at this time.", 0);
-
-      return;
-   }
-
-   // Get the device data
-   $sensor_data = bd_foobot_call_data_api();
-   if (is_wp_error($sensor_data)) {
-      error_log("Error: No data from Foobot sensor API ", 0);
-      return false; // Bail early
-   }
-
-   // Add the API to the database
-   $table_name = $wpdb->prefix . 'bd_foobot_sensor_data';
-   foreach ( $sensor_data as $key => $value ){
-      $wpdb->insert( $table_name, array( $key => $value ));
-   }
-   // vars
-   $device=$unitTmp=$datapointTmp='';
-
-   // Temperature
-   $unitTmp = $sensor_data->{"unitTmp"};
-   $datapointTmp = $sensor_data->{"datapointTmp"};
-
-   // Device
-   $device = $sensor_data->{"device"};
-
-   // Humidity
-   $unitHum = $sensor_data->{"unitHum"};
-   $datapointHum = $sensor_data->{"datapointHum"};
-
-   // CO2
-   $unitCo2 = $sensor_data->{"unitCo2"};
-   $datapointCo2 = $sensor_data->{"datapointCo2"};
-
-   // Voc
-   $unitVoc = $sensor_data->{"unitVoc"};
-   $datapointVoc = $sensor_data->{"datapointVoc"};
-
-   // All Pollution
-   $unitAllpollu = $sensor_data->{"unitAllpollu"};
-   $datapointAllpollu = $sensor_data->{"datapointAllpollu"};
-
-   // Get timestamp
-   $timestamp = $sensor_data->{"start"};
-   $time = date('Y-m-d H:i:s', $timestamp);
-
-   // Get the temperature
-   $datapoints = $sensor_data->{"datapoints"};
-   $datapoint = $datapoints[0];
-   $datapointTmp = $datapoint[2];
-
-   // Get the temperature units
-   $units = $sensor_data->{"units"};
-   $unitTmp = $units[2];
-
    /**
-    * Insert data into custom database table
+    * Request an API call
+    * (checks if transient set, if
+    * not, makes API call)
     */
+      $data = bd_foobot_get_sensor_data();
 
-
-   /**
-    * 
-   $wpdb->insert(
-      $table_name,
-      array(
-         'time'                  => $time,
-         'device'                => $device,
-         'unitPm'                => $sensor_data->{"unitPm"},
-         'datapointPm'           => $sensor_data->{"datapointPm"},
-         'unitTmp'               => $unitTmp,
-         'datapointTmp'          => $datapointTmp,
-         'unitHum'               => $unitHum,
-         'datapointHum'          => $datapointHum,
-         'unitCo2'               => $unitCo2,
-         'datapointCo2'          => $datapointCo2,
-         'unitVoc'               => $unitVoc,
-         'datapointVoc'          => $datapointVoc,
-         'unitAllpollu'          => $unitAllpollu,
-         'datapointAllpollu'     => $datapointAllpollu,
-      )
-   ); 
-   **/
-
-   // echo $wpdb->show_errors();
-
-   // Transient is set for 5 mins
-   set_transient('foobot-api-data-updated', 1, (60 * 5));
-
-   // Debug
-   error_log("Foobot sensor data has been updated! Next update > 5 mins.", 0);
+      if( $data ){
+         /**
+          * If the request returns data
+          * (i.e. transient not set)
+          * update the database
+          */
+         bd_foobot_update_db_sensor( $data );
+      }
 }
 
 /**
