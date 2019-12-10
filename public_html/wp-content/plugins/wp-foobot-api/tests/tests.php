cbeb3e7e525f72ae182d5b81da8a33e7f70eb4ae
Mark <Bain>
2019-12-10 18:00:07 +0100

    output sensor markup

--- public_html/wp-content/plugins/wp-foobot-api/tests/tests.php
+++ public_html/wp-content/plugins/wp-foobot-api/tests/tests.php
@@ -10,21 +10,32 @@ function bdf_sc_test_fetch_api_device( $atts )
       'device' => '',
   ), $atts );
   
-  $device_name = $device_data["device"]; 
+  $device_name = $device_data["device"];
 
-  // $data = bd_foobot_call_data_api( $device_name );
-
-  $uuid = '2C03466F849044E0'; // test (BainBot)
-  $data = bdf_query_sensors( $uuid );
-  // bd_pretty_debug( $data, "data" );
+  // Get the device UUID
+  $uuid = bd_get_foobot_device_uuid( $device_name );
+  bd_pretty_debug( $uuid, "uuid" );
+  
+  // Fetch the sensor data from the database
+  $sensor_data = bdf_query_sensors( $uuid );
+  $data = $sensor_data[0];
+  bd_pretty_debug( $data, "data" );
 
   // Add the data to the database
   // bd_foobot_update_db_sensors( $data );
 
-
-
-
+  // Output
   ob_start();
+  echo '<ul class="sensors">';
+  echo '<li class="sensor sensor--tmp"><span class="sensor__label">Temperature</span><span class="sensor__data">' . $data['datapointTmp'] . '</span><span class="sensor__unit">' . $data['unitTmp'] . '</span></li>' ;
+  echo '<li class="sensor sensor--pm"><span class="sensor__label">PM</span><span class="sensor__data">' . $data['datapointPm'] . '</span><span class="sensor__unit">' . $data['unitPm'] . '</span></li>' ;
+  echo '<li class="sensor sensor--co2"><span class="sensor__label">Co2</span><span class="sensor__data">' . $data['datapointCo2'] . '</span><span class="sensor__unit">' . $data['unitCo2'] . '</span></li>' ;
+  echo '<li class="sensor sensor--voc"><span class="sensor__label">VOC</span><span class="sensor__data">' . $data['datapointVoc'] . '</span><span class="sensor__unit">' . $data['unitVoc'] . '</span></li>' ;
+  echo '<li class="sensor sensor--hum"><span class="sensor__label">Humidity</span><span class="sensor__data">' . $data['datapointHum'] . '</span><span class="sensor__unit">' . $data['unitHum'] . '</span></li>' ;
+  echo '<li class="sensor sensor--all"><span class="sensor__label">All</span><span class="sensor__data">' . $data['datapointAllpollu'] . '</span><span class="sensor__unit">' . $data['unitAllpollu'] . '</span></li>' ;
+  echo '</ul>';
+
+  
   $content =  ob_get_contents();
   ob_clean();
   return $content;
